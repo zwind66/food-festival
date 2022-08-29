e6993293710604501a4c0a2cc1eb90eb608bd497
Shuai Zuo <szuo1@vols.utk.edu>
2022-08-28 20:38:22 -0500

    update again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-require("bootstrap");
-const img1 = require("../../assets/img/food-table.jpg");
-const img2 = require("../../assets/img/grill.jpg");
+require('bootstrap');
+const img1 = require("../img/food-table.jpg");
+const img2 = require("../img/grill.jpg");
 
-$(document).ready(function () {
-
+$(document).ready(function() {
   // First image is hard coded in index.html
   const carouselSlides = [
     {
@@ -20,8 +19,8 @@ $(document).ready(function () {
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
-  ]
-
+  ];
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
